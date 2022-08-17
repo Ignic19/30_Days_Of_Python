f49949058076499a18beda6612f202272ed7e4a8
Ignacio Nicolas Canales <ignacionicolas_1996@outlook.com>
2022-08-17 17:06:47 -0400

    Add files via upload

--- 30DaysOfPython/Day_6/tuples.py
+++ 30DaysOfPython/Day_6/tuples.py
@@ -3,9 +3,15 @@
 # ### Exercises: Level 1
 
 # 1. Create an empty tuple
+tupla_vacia = ()
 # 2. Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+tupla_nombres_hermanos = ('Dylann', 'Isaias', 'Ashley', 'Antonella', 'Nicolas')
+print(tupla_nombres_hermanos)
 # 3. Join brothers and sisters tuples and assign it to siblings
+hermanos = tuple(tupla_nombres_hermanos)
+print(hermanos)
 # 4. How many siblings do you have?
+print(len(tupla_nombres_hermanos))
 # 5. Modify the siblings tuple and add the name of your father and mother and assign it to family_members
 
 # ### Exercises: Level 2
@@ -16,7 +22,7 @@
 # 1. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
 # 1. Slice out the first three items and the last three items from food_staff_lt list
 # 1. Delete the food_staff_tp tuple completely
-# 1. Check if an item exists in  tuple:
+# 1. Check if an item exists in tuple:
 
 # - Check if 'Estonia' is a nordic country
 # - Check if 'Iceland' is a nordic country
