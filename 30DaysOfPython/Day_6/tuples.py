64cc6f592c3d3a81dd22f135d01dde33968e9b64
Ignacio Nicolas Canales <ignacionicolas_1996@outlook.com>
2022-08-17 17:16:49 -0400

    Add files via upload

--- 30DaysOfPython/Day_6/tuples.py
+++ 30DaysOfPython/Day_6/tuples.py
@@ -13,20 +13,21 @@ print(hermanos)
 # 4. How many siblings do you have?
 print(len(tupla_nombres_hermanos))
 # 5. Modify the siblings tuple and add the name of your father and mother and assign it to family_members
-
+padres = ('Rosa', 'Juan')
+familia = tupla_nombres_hermanos + padres
+print( familia)
 # ### Exercises: Level 2
 
 # 1. Unpack siblings and parents from family_members
-# 1. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
-# 1. Change the about food_stuff_tp  tuple to a food_stuff_lt list
-# 1. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
-# 1. Slice out the first three items and the last three items from food_staff_lt list
-# 1. Delete the food_staff_tp tuple completely
-# 1. Check if an item exists in tuple:
+# 2. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+# 3. Change the about food_stuff_tp  tuple to a food_stuff_lt list
+# 4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+# 5. Slice out the first three items and the last three items from food_staff_lt list
+# 6. Delete the food_staff_tp tuple completely
+# 7. Check if an item exists in tuple:
+
+
+#   nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
 
 # - Check if 'Estonia' is a nordic country
 # - Check if 'Iceland' is a nordic country
-
-#   ```py
-#   nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
-#   ```
